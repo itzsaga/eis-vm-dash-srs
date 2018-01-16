592aa923b88047cc3912326a9477b86841bda80f
Seth <itz.saga@gmail.com>
2018-01-16 17:46:25 -0600

    Update production config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -28,6 +28,16 @@ module.exports = () => ({
           name: '[name].[ext]',
         },
       },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }],
+      }
     ],
   },
   plugins: [
