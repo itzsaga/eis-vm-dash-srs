c00f71708badd693c25ccdfe243f22ca75e40214
Seth <itz.saga@gmail.com>
2018-01-16 15:44:01 -0600

    Sort state based on VM name

--- src/App.js
+++ src/App.js
@@ -31,6 +31,11 @@ class App extends Component {
           client: vms[vm].client
         });
       }
+      newState.sort((a, b) => {
+        const x = a.vm.toLowerCase();
+        const y = b.vm.toLowerCase();
+        return x < y ? -1 : x > y ? 1 : 0;
+      })
       this.setState({
         vms: newState
       });
